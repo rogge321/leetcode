901e993fdcdb128bd99263c3ff0540f3bbff52a5
zhengpanlong <zhengpanlong@kuaishou.com>
2017-11-25 11:32:56 +0800

    init version

---
+++ src/main/java/com/panlong/Main.java
@@ -0,0 +1,29 @@
+package com.panlong;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by rogge on 2017/4/3.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This is the first project!");
+
+        List<String> test = new ArrayList<String>();
+
+        test.add("123");
+
+        System.out.println(test);
+
+        ListTest(test);
+
+        System.out.println(test);
+    }
+
+
+    public static void ListTest(final List<String> test) {
+        test.clear();
+
+    }
+}
